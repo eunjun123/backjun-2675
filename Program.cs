00ebdd1cdcd36c09a2eeb183e3842f1f3b1e76b3
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 18:27:26 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int trial = int.Parse(ReadLine());
+            for (int i = 0; i < trial; i++)
+            {
+                string[] input = ReadLine().Split();
+                int n = int.Parse(input[0]);
+                string word = input[1];
+                for (int j = 0; j < word.Length; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        Write(word[j]);
+                    }
+                }
+                Write("\n");
+            }
+        }
+    }
